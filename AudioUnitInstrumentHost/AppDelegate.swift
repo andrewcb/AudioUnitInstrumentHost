f3523f530e3a0a7ca056b28eb67faf2507293cc9
acb <acb@staticplain.local>
2017-12-07 16:23:46 +0000

    Initial Commit

---
+++ AudioUnitInstrumentHost/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  AudioUnitInstrumentHost
+//
+//  Created by acb on 07/12/2017.
+//  Copyright © 2017 acb. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ AudioUnitInstrumentHost/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AudioUnitInstrumentHost
+//
+//  Created by acb on 07/12/2017.
+//  Copyright © 2017 acb. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
